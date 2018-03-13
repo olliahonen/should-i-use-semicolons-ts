8bc7a42d9f01c882af050b48de6c0e0c79ab96ab
Olli Ahonen <olliahonen>
2018-03-13 16:30:56 +0200

    Add third example

---
+++ example3.ts
@@ -0,0 +1,19 @@
+import Bike from './bike';
+
+const bikesForSale = {'red': ['Cannondale', 'Focus'], 'blue': ['Giant']};
+
+export function example3() {
+  const mikesGarage = bikesForSale
+
+  ['purple', 'red'].forEach(color => (new Bike(color)).pedal())
+
+  console.log(mikesGarage)
+}
+
+export function example3WithSemis() {
+  const mikesGarage = bikesForSale;
+
+  ['purple', 'red'].forEach(color => (new Bike(color)).pedal());
+
+  console.log(mikesGarage);
+}
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import {example1, example1WithSemis} from './example1';
 import {example2, example2WithSemis} from './example2';
+import {example3, example3WithSemis} from './example3';
 
 console.log('\nEXAMPLE 1');
 try {
@@ -20,3 +21,9 @@ example2();
 
 console.log('\nEXAMPLE 2 WITH SEMICOLONS');
 example2WithSemis();
+
+console.log('\nEXAMPLE 3');
+example3();
+
+console.log('\nEXAMPLE 3 WITH SEMICOLONS');
+example3WithSemis();
