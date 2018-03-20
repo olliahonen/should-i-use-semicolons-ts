3d8a11f761341aa9c1b44f7b27ffb3e1bf995b50
Olli Ahonen <olliahonen>
2018-03-20 10:29:00 +0200

    Remove a pointless example

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import {example1, example1WithSemis} from './example1';
 import {example2, example2WithSemis} from './example2';
-import {example3, example3WithSemis} from './example3';
 
 console.log('\nEXAMPLE 1');
 try {
@@ -17,9 +16,3 @@ example2();
 
 console.log('\nEXAMPLE 2 WITH SEMICOLONS');
 example2WithSemis();
-
-console.log('\nEXAMPLE 3');
-example3();
-
-console.log('\nEXAMPLE 3 WITH SEMICOLONS');
-example3WithSemis();
